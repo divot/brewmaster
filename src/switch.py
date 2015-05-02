fed015df0463d10e8dcccfc59467fc1682d2fdba
Daniel Powell <divot.powell@googlemail.com>
2015-05-02 22:11:06 +0100

    Converted switch to be a component container, and added timed on/off

--- src/switch.py
+++ src/switch.py
@@ -1,29 +1,45 @@
 import abc
 import RPi.GPIO as GPIO
+import component
+import threading
 
-class Switch(object):
+class Switch(component.ComponentContainer):
     __metaclass__ = abc.ABCMeta
 
-    def __init__(self, devices = None):
-        self.devices = devices
-
-    def get_devices(self):
-        return devices
+    def __init__(self):
+        self.timer = None
 
     @abc.abstractmethod
     def switch_on(self):
         pass
 
     @abc.abstractmethod
-    def switch_on(self):
+    def switch_off(self):
         pass
+
+    def timed_on(self, time):
+        if self.timer:
+            self.timer.cancel()
+            del self.timer
+        else:
+            self.switch_on()
+        self.timer = threading.Timer(time, self.__timer_func)
+        self.timer.start()
+
+    def timed_cancel(self):
+        if self.timer:
+            self.__timer_func()
+
+    def __timer_func(self):
+        self.switch_off()
+        self.timer = None
 
 class GPIOSwitch(Switch):
 
     GPIO.setmode(GPIO.BCM)
 
-    def __init__(self, port, devices = None):
-        super(GPIOSwitch, self).__init__(devices)
+    def __init__(self, port):
+        Switch.__init__(self)
         self.port = port
         GPIO.setup(self.port, GPIO.OUT)
         GPIO.output(self.port, GPIO.LOW)
